238a23a19ea9eaffaa0c3d4059030c530b697dd0
Anas CHERIET <anascheriet@hotmail.com>
2021-03-31 00:32:25 +0100

    Approve/Reject Payment

--- src/main/java/com/acheriet/microservices/kafka_saga_pattern/paymentService/config/PaymentServiceConfig.java
+++
@@ -1,7 +0,0 @@
-package com.acheriet.microservices.kafka_saga_pattern.paymentService.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PaymentServiceConfig {
-}
--- src/main/java/com/acheriet/microservices/kafka_saga_pattern/paymentService/eventHandlers/OrderEventProcessorService.java
+++ src/main/java/com/acheriet/microservices/kafka_saga_pattern/paymentService/eventHandlers/OrderEventProcessorService.java
@@ -1,27 +1,26 @@
 package com.acheriet.microservices.kafka_saga_pattern.paymentService.eventHandlers;
 
+import com.acheriet.microservices.kafka_saga_pattern.models.enums.OrderStatus;
 import com.acheriet.microservices.kafka_saga_pattern.models.enums.PaymentStatus;
 import com.acheriet.microservices.kafka_saga_pattern.models.events.OrderEvent;
 import com.acheriet.microservices.kafka_saga_pattern.models.events.PaymentEvent;
+import com.acheriet.microservices.kafka_saga_pattern.orderService.repository.PurchaseOrderRepository;
 import lombok.var;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 @Service
 public class OrderEventProcessorService {
+
+    @Autowired
+    private PurchaseOrderRepository purchaseOrderRepository;
 
     // user - credit limit
     public static final Map<Integer, Integer> userMap = new HashMap<>();
@@ -41,17 +40,45 @@ public class OrderEventProcessorService {
         if (creditLimit >= price) {
             paymentEvent.setStatus(PaymentStatus.APPROVED);
             userMap.computeIfPresent(orderEvent.getUserId(), (k, v) -> v - price);
+            purchaseOrderRepository.findById(orderEvent.getOrderId()).map(ord -> {
+                ord.setStatus(OrderStatus.ORDER_COMPLETED);
+                return purchaseOrderRepository.save(ord);
+            });
         }
         else {
             paymentEvent.setStatus(PaymentStatus.REJECTED);
+            purchaseOrderRepository.findById(orderEvent.getOrderId()).map(ord -> {
+                ord.setStatus(OrderStatus.ORDER_CANCELLED);
+                return purchaseOrderRepository.save(ord);
+            });
         }
         return paymentEvent;
+    }
+
+    public OrderEvent consumerOutputToOrderEvent(String message) {
+        Map<String, Integer> order = new HashMap<>();
+        var arr = message.split("\\{")[1];
+        arr = arr.split("}")[0];
+
+        var tab = arr.split(",");
+
+        var orderEvent = new OrderEvent();
+        orderEvent.setOrderId(Integer.parseInt(tab[0].split("=")[1]));
+        orderEvent.setPrice(Integer.parseInt(tab[2].split("=")[1]));
+        orderEvent.setUserId(Integer.parseInt(tab[1].split("=")[1]));
+
+        /*
+         * for (String x : tab) { order.put(x.split("=")[0].replace(" ", ""),
+         * Integer.parseInt(x.split("=")[1])); }
+         */
+        return orderEvent;
     }
 
     @KafkaListener(topics = "saga-topic", groupId = "saga_app")
     public void consumeOrderEvents(String message) {
         Logger logger = LoggerFactory.getLogger(OrderEventProcessorService.class);
         logger.info(String.format("#### -> Consumed Event -> %s", message));
+        logger.info(processOrderEvent(consumerOutputToOrderEvent(message)).toString());
     }
 
 }
